ae8741aeaa7cdabe19f6d9bed67aa0b1092c7b8c
Vanessa Soares de Morais <vsmdantas@outlook.com>
2022-03-22 16:14:20 -0300

    Adicionar arquivos de projeto.

---
+++ 6_AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 6");
+
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+
+            Console.WriteLine(idadeGustavo);
+
+            Console.ReadLine();
+        }
+    }
+
+}
